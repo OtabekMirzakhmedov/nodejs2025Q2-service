8c7694fba8c42ce66b706783fc3ff5ea1f32e732
OtabekMirzakhmedov <otabek.mirzakhmedov@gmail.com>
2025-05-29 20:43:58 +0500

    artist module created

---
+++ src/artists/artists.controller.ts
@@ -0,0 +1,56 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Put,
+  Param,
+  Delete,
+  HttpCode,
+  HttpStatus,
+  BadRequestException,
+} from '@nestjs/common';
+import { validate as isUUID } from 'uuid';
+import { ArtistsService } from './artists.service';
+import { CreateArtistDto } from './dto/create-artist.dto';
+import { UpdateArtistDto } from './dto/update-artist.dto';
+
+@Controller('artist') // Changed from 'artists' to 'artist'
+export class ArtistsController {
+  constructor(private readonly artistsService: ArtistsService) {}
+
+  @Post()
+  create(@Body() createArtistDto: CreateArtistDto) {
+    return this.artistsService.create(createArtistDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.artistsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    if (!isUUID(id)) {
+      throw new BadRequestException('Invalid UUID format');
+    }
+    return this.artistsService.findOne(id);
+  }
+
+  @Put(':id') // Changed from @Patch to @Put
+  update(@Param('id') id: string, @Body() updateArtistDto: UpdateArtistDto) {
+    if (!isUUID(id)) {
+      throw new BadRequestException('Invalid UUID format');
+    }
+    return this.artistsService.update(id, updateArtistDto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(@Param('id') id: string) {
+    if (!isUUID(id)) {
+      throw new BadRequestException('Invalid UUID format');
+    }
+    return this.artistsService.remove(id);
+  }
+}
---
+++ src/artists/artists.service.ts
@@ -0,0 +1,51 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { randomUUID } from 'crypto';
+import { CreateArtistDto } from './dto/create-artist.dto';
+import { UpdateArtistDto } from './dto/update-artist.dto';
+import { Artist } from './entities/artist.entity';
+
+@Injectable()
+export class ArtistsService {
+  private artists: Artist[] = [];
+
+  findAll(): Artist[] {
+    return this.artists;
+  }
+
+  findOne(id: string): Artist {
+    const artist = this.artists.find((artist) => artist.id === id);
+    if (!artist) {
+      throw new NotFoundException(`Artist with id ${id} not found`);
+    }
+    return artist;
+  }
+
+  create(createArtistDto: CreateArtistDto): Artist {
+    const newArtist: Artist = {
+      id: randomUUID(),
+      name: createArtistDto.name,
+      grammy: createArtistDto.grammy,
+    };
+
+    this.artists.push(newArtist);
+    return newArtist;
+  }
+
+  update(id: string, updateArtistDto: UpdateArtistDto): Artist {
+    const artist = this.findOne(id); // Will throw NotFoundException if not found
+
+    artist.name = updateArtistDto.name;
+    artist.grammy = updateArtistDto.grammy;
+
+    return artist;
+  }
+
+  remove(id: string): void {
+    const artistIndex = this.artists.findIndex((artist) => artist.id === id);
+    if (artistIndex === -1) {
+      throw new NotFoundException(`Artist with id ${id} not found`);
+    }
+
+    this.artists.splice(artistIndex, 1);
+  }
+}
